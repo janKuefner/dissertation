cb9de6113b9e0ed99c4d4461724f6f4bd699f9ff
janku <jan.kuefner@gmx.de>
2023-01-05 16:58:26 +0100

    hardware hacking device funzt, kann jeden Aktuator einmal schalten

--- HdwHckngDvc/main.py
+++ HdwHckngDvc/main.py
@@ -5,16 +5,27 @@ import time  # necessary for I2 communication
 def initialize_I2Cs():
     #  initializing I2C adress 0x20
     '''I2C address 0x20, side A, GPIOs set to output'''
+    i2cbus.write_word_data(0x20, 0x00, 0x00)
+    '''I2C address 0x20, side B, GPIOs set to output'''
+    i2cbus.write_word_data(0x20, 0x01, 0x00)
+    #  initializing I2C adress 0x21
+    '''I2C address 0x20, side A, GPIOs set to output'''
     i2cbus.write_word_data(0x21, 0x00, 0x00)
     '''I2C address 0x20, side B, GPIOs set to output'''
     i2cbus.write_word_data(0x21, 0x01, 0x00)
+    #  initializing I2C adress 0x22
+    '''I2C address 0x20, side A, GPIOs set to output'''
+    i2cbus.write_word_data(0x22, 0x00, 0x00)
+    '''I2C address 0x20, side B, GPIOs set to output'''
+    i2cbus.write_word_data(0x22, 0x01, 0x00)
 
 
 def switch_module_pin(module, pin):
-    # general times
-    time_actuator_on = 0.1   # time the actuator is powered
-    time_actuator_off = 0.001  # time the actuator is off
-    # provide the module I2C adress as hex value
+    # general times the actuator stays actived / deactiveted
+    time_actuator_on = 0.1   # time the actuator is powered. 0.1 is good
+    time_actuator_off = 0.001  # time the actuator is off. 0.001 is good
+    '''the following code switches on and off a certain actuator'''
+    # get the module I2C adress as hex value
     if module == 1:
         i2caddress = 0x20
     elif module == 2:
@@ -23,7 +34,9 @@ def switch_module_pin(module, pin):
         i2caddress = 0x22
     else:
         print("error")
-    # provide pin and side as hex value
+    # get the pin and side as hex value
+    # A side = 0x12
+    # B side = 0x15
     if pin == 0:
         pin_hex = 0x00
         A_or_B_side = 0x12
@@ -77,6 +90,7 @@ def switch_module_pin(module, pin):
         A_or_B_side = 0x15
     else:
         raise ValueError("This pin number is not defined")
+    '''send hex values to chip in order to switch on the actuactor'''
     sent = False
     while (sent == False):
         try:
@@ -86,6 +100,7 @@ def switch_module_pin(module, pin):
             sent = True
         except OSError:
             sent = False
+    ''' switch off A and B side'''
     sent = False
     while (sent == False):
         try:
@@ -104,38 +119,23 @@ def switch_module_pin(module, pin):
             sent = False
 
 
+def switch_every_actuator_once():
+    '''this function switches all actuators once. This function is sort of to
+    help setting up or troubleshooting the hardware.'''
+    # module 1
+    for m in range(1, 4):
+        for p in range(1, 17):
+            print(m, p)
+            switch_module_pin(m, p)
+
+
+def hacktype(string_to_hacktype):
+    print(len(string_to_hacktype))
+
+
 if __name__ == "__main__":
     i2cbus = SMBus(1)  # i2cbus object creation
     initialize_I2Cs()
     while (True):
-        # module 1
-        switch_module_pin(1, 1)
-        switch_module_pin(1, 2)
-        switch_module_pin(1, 3)
-        switch_module_pin(1, 4)
-        switch_module_pin(1, 5)
-        switch_module_pin(1, 6)
-        switch_module_pin(1, 7)
-        switch_module_pin(1, 8)
-        switch_module_pin(1, 9)
-        switch_module_pin(1, 10)
-        switch_module_pin(1, 11)
-        switch_module_pin(1, 12)
-        switch_module_pin(1, 13)
-        switch_module_pin(1, 14)
-        switch_module_pin(1, 15)
-        switch_module_pin(1, 16)
-        switch_module_pin(1, 0)  # no switching at all
-        switch_module_pin(1, 0)  # no switching at all
-        switch_module_pin(1, 0)  # no switching at all
-        switch_module_pin(1, 0)  # no switching at all
-        # module 2
-        switch_module_pin(2, 1)
-        switch_module_pin(2, 2)
-        switch_module_pin(2, 3)
-        switch_module_pin(2, 4)
-        switch_module_pin(2, 5)
-        switch_module_pin(2, 6)
-        switch_module_pin(2, 7)
-        switch_module_pin(2, 8)
-        switch_module_pin(2, 9)
+        switch_every_actuator_once()
+        hacktype("yolo")
