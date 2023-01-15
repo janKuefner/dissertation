797c28069a9316275976b65abbae6fea1e51cebd
janku <jan.kuefner@gmx.de>
2023-01-15 13:20:01 +0100

    OCR_camera working

--- HdwHckngDvc/OCR.py
+++
@@ -1,6 +0,0 @@
-import easyocr
-
-print("yolo")
-reader = easyocr.Reader(['en'])  # this needs to run only once to load the model into memory
-result = reader.readtext('test.jpg')
-print(result)
--- HdwHckngDvc/OCR_camera.py
+++ HdwHckngDvc/OCR_camera.py
@@ -1,13 +1,17 @@
 from picamera2 import Picamera2, Preview
-
+import easyocr
 import time
+
 picam2 = Picamera2()
 # camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
 # picam2.configure(camera_config)
-print("yolo-1")
-
 picam2.start_preview(Preview.QTGL)
-print("yolo-2")
 picam2.start()
 time.sleep(2)
-picam2.capture_file("test.jpg")
+picam2.capture_file("images/test.jpg")
+print("picture taken")
+'''##################################'''
+print("starting OCR...")
+reader = easyocr.Reader(['en'])  # this needs to run only once to load the model into memory
+result = reader.readtext('images/test.jpg')
+print(result)
