3fa6b6114b46fa852b62092ed61480a72344ae6a
janku <jan.kuefner@gmx.de>
2023-03-05 18:56:00 +0100

    working :)

--- hack_device/app.py
+++ hack_device/app.py
@@ -1,32 +1,44 @@
-from picamera2 import Picamera2, Preview  # for taking pictures of the result
-import easyocr  # https://github.com/JaidedAI/EasyOCR
-from modules.robo_typer import Robo_typer
-from time import sleep
+from picamera2 import Picamera2, Preview  # import library to use Pi`s webcam 
+import easyocr  # import AI image to text transformer
+from modules.robo_typer import Robo_typer  # import the Robo_typer object
 
 
-class Hack_device(Robo_typer):
+class Hack_device(Robo_typer, Picamera2):
     def __init__(self):
-        self.robo_typer = Robo_typer()
-        self.is_password_cracked = False
-        self.file_in_memory = open("wordlists/fasttrack.txt", "r")
+        self.robo_typer = Robo_typer()  # create robo_Typer object
+        self.picam2 = Picamera2()  # create camera object
+        self.reader = easyocr.Reader(['en'], gpu=False)  # create easyocr objct
+        self.wrdlst = open("wordlists/fasttrack.txt", "r")  # wordlist location
 
     def bruteforce(self):
+        '''this method is the bruteforcing algorithm'''
         self.robo_typer.initialize_I2Cs()  # initalize I2C
-        for line in self.file_in_memory:
-            print("I am typing:", line)
+        self.picam2.start_preview(Preview.QTGL)  # open a preview window
+        self.picam2.start()  # start camera for further processing
+        f = open("results.txt", "w")  # open results file in write mode
+        f.write("")  # write nothing in the file / delete content
+        f.close()  # close file
+        for line in self.wrdlst:  # try every line entry in the wordlist
+            print("\nI am typing:", line.strip('\n'))  # info for operator
+            '''the following let`s the hack device type the line entry of the 
+            wordlist. In other words: The hacking device is typing one 
+            password'''
             self.robo_typer.type_string(line.strip('\n') + "   ")
-            # picam2.capture_file("images/image01.jpg")  # picture size = 640 x 480
-            # sleep(4)
-            # result = reader.readtext('images/image01.jpg', detail=0)
-            # print(result)
+            print("Taking photo")  # info for operator
+            self.picam2.capture_file("images/image01.jpg")  # take picture
+            print("transferring photo to text") # info for operator
+            '''the following AI converts the image to text'''
+            result = self.reader.readtext('images/image01.jpg', detail=0)
+            result_str = " ".join(result)  # merge the result list to a string
+            print("This is what I read", result) # info for operator
+            f = open("results.txt", "a")  # open file for appending
+            '''the following concenacts what was typed and the result text,
+            which was AI converted from the camera'''
+            new_line = line.strip('\n') + " - " + result_str + "\n"
+            f.write(new_line)  # append the new line
+            f.close()  # close file
 
 
 if __name__ == "__main__":
     hack_device = Hack_device()  # create a hack_device object
-    hack_device.bruteforce()
-    # picam2 = Picamera2()
-    # picam2.start_preview(Preview.QTGL)
-    # picam2.start()
-    # reader = easyocr.Reader(['en'], gpu=False)  # load AI model into memory
-    
-    
+    hack_device.bruteforce()  # start the bruteforce method
