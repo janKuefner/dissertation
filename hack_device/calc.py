f49c0599d360926b906b59e1e757f77951868cd6
janku <jan.kuefner@gmx.de>
2023-03-08 13:40:40 +0100

    unittest kinda works

--- hack_device/calc.py
+++ hack_device/calc.py
@@ -1,4 +1,4 @@
-from smbus2 import SMBus
+import json
 
 
 def add(x, y):
@@ -6,9 +6,23 @@ def add(x, y):
     return x + y
 
 
-def yolo():
-    print(yolo2.write_byte_data(32, 0, 0))
+def switch_row_actuator(row, actuator):
+    with open('modules/electronics_to_mechanics.json') as json_file:
+        e_to_m_data = json.load(json_file)
+        data_entry_found = False
+    for x in range(40):
+        if (((e_to_m_data[x]["row"] == row and
+                e_to_m_data[x]["actuator"] == actuator))):
+            # self.switch_module_outlet((e_to_m_data[x]["module"]),
+            #                             (e_to_m_data[x]["outlet"]))
+            data_entry_found = True
+            return ("yolo")
+    if data_entry_found is False:
+        raise ValueError("row #", row, "actuator #", actuator, " combination is not defined")
+
+# def yolo():
+#    print(yolo2.write_byte_data(32, 0, 0))
 
 
-yolo2 = SMBus(1)
-yolo()
+# yolo2 = SMBus(1)
+# yolo()
--- hack_device/test_calc.py
+++ hack_device/test_calc.py
@@ -9,6 +9,10 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.add(0, 1), 1)
         self.assertEqual(calc.add(-3, 3), 0)
 
+    def test_switch_row_actuator(self):
+        self.assertEqual(calc.switch_row_actuator(1, 1), "yolo")
+        self.assertRaises(Exception, calc.switch_row_actuator, 0, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
